e41dca1a90b2e964ce7d5cfcb7ec120557ef9284
Enderoxxo <enderajm@gmail.com>
2023-11-08 00:31:45 -0500

    Added Health

---
+++ MainChar.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+@onready var health = 9
+@onready var bodies
+@onready var DAMAGE_TAKEN = 3 # THIS is the number we change to balance damage
+var deathnotif = 0
+
+func update_hbar():
+	$HBar.value = health
+	
+func take_damage(damage):
+	health -= damage
+	update_hbar()
+
+func _on_rigid_body_2d_damage():
+	take_damage(DAMAGE_TAKEN)
+	pass # Replace with function body.
+
+		
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	update_hbar()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+func _process(delta):
+	
+	if health <= 0:
+		if deathnotif == 0:
+			print("You Died!")
+			GameManager.GameState.Lose
+			deathnotif = 1
+	pass
+
+
---
+++ MainCharCollision.gd
@@ -0,0 +1,24 @@
+extends RigidBody2D
+
+@onready var bodies
+@onready var currentball
+signal damage
+
+func try_collide():
+	bodies = get_colliding_bodies()
+	for body in bodies:
+		if currentball != body:
+			if body.is_in_group("Bullet2"): #check colliding body is in the "players" group
+				currentball = body
+				damage.emit() #whatever u want to do (i.e, damage the player)
+			
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	try_collide()
+	pass
